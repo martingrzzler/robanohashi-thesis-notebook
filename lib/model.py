3c3492da1ad0d1f4571b1e4733d09f59bc8e4b12
Martin Gressler <martin.gressler@code.berlin>
2023-03-31 13:39:24 +0200

    predict to lib

--- lib/model.py
+++ lib/model.py
@@ -12,3 +12,15 @@ class CLIPForRegression(CLIPTextModelWithProjection):
         outputs = self.dropout(outputs.text_embeds)
         outputs = self.regressor(outputs)
         return outputs
+
+
+def clip_predict_word(word, tokenizer, model, device=torch.device('cpu')):
+    with torch.no_grad():
+        outputs = model(**tokenizer(word, padding='max_length', return_tensors="pt", max_length=10).to(device))
+    return outputs.item()
+
+def bert_predict_word(word, tokenizer, model, device=torch.device('cpu')):
+    input_ids = tokenizer(word, return_tensors='pt').input_ids.to(device)
+    with torch.no_grad():
+        outputs = model(input_ids)
+    return outputs.logits.item()
